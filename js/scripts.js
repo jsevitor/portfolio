bb353f02fb1954bbcd17a1578a436f423c99e8cd
voliverx <jose.vitor@estudante.iftm.edu.br>
2024-03-07 13:18:51 -0300

    setting mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function switchMenu() {
     let nav_menu = document.getElementById("nav_menu");
 
-    if (nav_menu.style.display == "block") {
+    if (nav_menu.style.display == "flex") {
         nav_menu.style.display = "none";
     } else {
-        nav_menu.style.display = "block";
+        nav_menu.style.display = "flex";
     }
 }
