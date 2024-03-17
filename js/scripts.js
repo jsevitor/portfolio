11bb2108d6f66b248eb6ed44a21f94961f109bcd
José Vitor Oliveira Reis <vitorjseo@gmail.com>
2024-03-17 18:09:12 -0300

    dark and light modes implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,64 @@
+const screen = window.innerWidth;
 let menu = document.getElementById("menu");
 let headerMenu = document.getElementById("header-menu");
 let itemMenu = document.querySelectorAll(".item-menu");
 let hero = document.querySelector(".hero");
 
+let themeBtn = document.querySelector(".theme-icon");
+let body = document.querySelector("body");
+let sectionBg = document.querySelectorAll(".section-bg");
+
+themeBtn.addEventListener('click', () => {
+    if (themeBtn.value == "1") {
+        themeBtn.value = "-1";
+        themeBtn.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+        body.classList.toggle("dark-mode");
+
+        sectionBg.forEach(item => {
+            item.classList.toggle("dark-bg");
+        })
+
+        if (screen <= 768) {
+            headerMenu.style.backgroundColor = "#242424";
+        }
+    } else {
+        themeBtn.value = "1";
+        themeBtn.innerHTML = `<i class="fa-solid fa-moon"></i>`; 
+        body.classList.remove("dark-mode");
+
+        sectionBg.forEach(item => {
+            item.classList.remove("dark-bg");
+        })
+
+        if (screen <= 768) {
+            headerMenu.style.backgroundColor = "#ECE1C1";
+        }
+    }
+})
 
 menu.addEventListener('click', () => {
     
     if (headerMenu.style.display == "flex") {
         headerMenu.style.display = "none";
         menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        themeBtn.style.display = "none";
     } else {
         headerMenu.style.display = "flex";
         menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        themeBtn.style.display = "block";
     }
 })
 
 hero.addEventListener('click', () => {
     headerMenu.style.display = "none";
     menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    themeBtn.style.display = "none";
 })
 
 itemMenu.forEach(item => {
     item.addEventListener('click', () => {
         headerMenu.style.display = "none";
+        themeBtn.style.display = "none";
         menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     });
 });
